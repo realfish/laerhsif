9f757a1f42aefae7fa8cd65480813ebcd3c251f4
Zhengyu Qian <laerhsif@gmail.com>
2019-12-20 19:55:48 +0800

    Add new polyfill & update libs.js

--- src/javascript/_libs.js
+++ src/javascript/_libs.js
@@ -1,4 +1,8 @@
-/**
+/*!
  * @license
- * jQuery v3.0.0 | (c) jQuery Foundation | jquery.org/license
+ * requestAnimationFrame polyfill | Erik Möller, et al. | MIT license | https://gist.github.com/paulirish/1579671
+ * DOM4 | Andrea Giammarchi @WebReflection | MIT license | https://github.com/WebReflection/dom4
  */
+
+// @codekit-append "polyfill/requestAnimationFrame.js"
+// @codekit-append "polyfill/dom4.max.js"
